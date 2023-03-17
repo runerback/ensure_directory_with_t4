c3bc9454d6f40c053e745fbb94e06caf2251af12
runerback <runerback@qq.com>
2023-03-17 22:40:24 +0800

    finish

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Mono.TextTemplating;
+
+var generator = new TemplateGenerator();
+
+var session = generator.GetOrCreateSession();
+
+var output = $"AutoGen/{Guid.NewGuid():N}/demo";
+
+session["output"] = Path.GetDirectoryName(output);
+
+foreach (var item in Enumerable.Range(0, 2))
+{
+    session["throwError"] = item > 0;
+
+    if (!await generator.ProcessTemplateAsync("template.tt", $"{output}_{item}"))
+    {
+        foreach (var error in generator.Errors)
+        {
+            Console.WriteLine($"error: {error}");
+        }
+    }
+}
